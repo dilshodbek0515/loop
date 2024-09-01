f95fda78ce52cd835c6c907ba9e3bba557df8fbe
dilshod <dilshodx516@gmail.com>
2024-09-01 12:14:57 +0500

    loop

---
+++ app.js
@@ -0,0 +1,139 @@
+//  For masala
+
+// N1
+// {
+//     let n = 10
+//     let k = 1
+
+//     for (let i = 0; i < n; i++) {
+//         console.log(k);
+//     }
+// }
+
+
+// N2
+// {
+//     let a = 1;
+//     let b = 10;
+//     let length = 0
+//     for (let i = a; i <= b; i++) {
+//         console.log(i);
+//         length++
+//     }
+//     console.log(`length:${length}`);
+// }
+
+
+
+// N3
+
+// {
+//     let a = 10;
+//     let b = 0;
+//     let length = 0
+//     for (let i = a - 1; i > b; i--) {
+//         console.log(i);
+//         length++
+//     }
+//     console.log(`length:${length}`);
+// }
+
+
+
+// N4
+
+// {
+//     let kg = 5;
+
+//     for (let i = 1; i <= 10; i++) {
+//         let money = kg * i;
+//         console.log(`${i} kg konfet narxi: ${money} so'm`);
+//     }
+// }
+
+
+
+
+// N5
+
+// {
+//     let kg = 5;
+
+//     for (let i = 0.1; i <= 1; i += 0.1) {
+//         let money = kg * i;
+//         console.log(`${i.toFixed(1)} kg konfet narxi: ${money.toFixed(2)} so'm`);
+//     }
+// }
+
+
+
+// N6
+
+// {
+//     let kg = 5;
+
+//     for (let i = 1.2; i <= 2.1; i += 0.1) {
+//         let money = kg * i;
+//         console.log(`${i.toFixed(1)} kg konfet narxi: ${money.toFixed(2)} so'm`);
+//     }
+// }
+
+
+
+// N7
+
+
+// {
+//     let a = 1
+//     let b = 10
+//     let c = 0
+
+//     for (let i = a; i <= b; i++) {
+//         c += i;
+//     }
+//     console.log(c);
+// }
+
+
+
+// N8
+
+// {
+//     let a = 1
+//     let b = 5
+//     let c = 1
+
+//     for (let i = a; i <= b; i++) {
+//         c *= i;
+//     }
+//     console.log(c);
+// }
+
+
+
+// N9
+
+// {
+//     let a = 5
+//     let b = 10
+//     let c = 0
+
+//     for (let i = a; i <= b; i++) {
+//         c += i * i;
+//     }
+//     console.log(c);
+// }
+
+
+// N10
+
+{
+    let n = 5;
+    let S = 0;
+
+    for (let i = 1; i <= n; i++) {
+        S += 1 / i;
+    }
+
+    console.log(S);
+}
